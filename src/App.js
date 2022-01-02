ad85fdd0e7e720cd06c7fc2e77b8f104c698d6a9
socrateskld <77156381+socrateskld@users.noreply.github.com>
2022-01-02 12:15:45 +0000

    Adding UseState in The First Container

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import data from "./data";
 import List from "./List";
 function App() {
+  const [people, setPeople] = useState(data);
+
   return (
     <main>
       <section className="container">
-        <h3>0 BirthDays Today </h3>
-        <List />
+        <h3>{people.length} BirthDays Today </h3>
+        <List people={people} />
         <button onClick={() => console.log(" You Click Me!")}>Clear All</button>
       </section>
     </main>
